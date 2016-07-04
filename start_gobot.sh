652b7523075d5352891bdbe10bf0ea6280d15b5a
Pierre Guinoiseau <pierre.guinoiseau@catalyst.net.nz>
2016-07-05 11:56:02 +1200

    Docker container improvements
    - Update to Ubuntu 16.04
    - No self-update with git, rebuilding the container is cleaner and gobot
      seldom gets updates
    - Move the code into /gobot, just for the sake of shorter paths
    - Fewer layers

--- start_gobot.sh
+++ start_gobot.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-
-# first, ensure git repo is up to date
-cd /home/iamgobot/gobot && git pull
-python gobot.py -j $JID -p $JPASSWD -r $JROOM -n $JNICK -g $GODOMAIN -s $GOSTAGES
+#!/bin/sh
+exec python /gobot/gobot.py -j $JID -p $JPASSWD -r $JROOM -n $JNICK -g $GODOMAIN -s $GOSTAGES
